db39317efdc22d6699484976e534e2e5bc8a620f
bsampaio <bsampaio@outlook.com>
2014-11-19 00:26:43 -0200

    Reconfigurando responsividade

--- colaborador-lista.php
+++ colaborador-lista.php
@@ -14,43 +14,45 @@
 ?>
 <h1>Lista de Colaboradores</h1>
 <br/>
-<table class="table table-striped table-condensed table-responsive">
-    <th>Nome</th>
-    <th>Email</th>
-    <th>Data de Nascimento</th>
-    <th>Tipo</th>
-    <th>Alterar</th>
-    <th>Remover</th>
-	<?php
-        if(!empty($colaboradores)):
-		foreach ($colaboradores as $colaborador) :
-	?>
-		<tr>
-			<td><?=$colaborador['nome']?></td>
-			<td><?=$colaborador['email']?></td>
-			<td><input type="date" value="<?=$colaborador['datanascimento']?>" readonly="true"/></td>
-			<td><?=$colaborador['tipo_nome']?></td>
-			<td>
-				<center>
-						<a class="btn btn-warning" href="edita-colaborador-formulario.php?id=<?=$colaborador['id']?>">
-				            <span class="glyphicon glyphicon-edit"></span>
-                        </a>
-                </center>
-			</td>
-            <td>
-				<form action="remove-colaborador.php" method="POST">
-					<center>
-						<input type="hidden" name="id" value="<?=$colaborador['id']?>">
-						<button class="btn btn-danger">
-							<span class="glyphicon glyphicon-remove"></span>
-						</button>
-					</center>
-				</form>
-			</td>
-		</tr>
-	<?php
-		endforeach;
-        endif;
-	?>
-</table>
+<div class="table-responsive">
+    <table class="table table-striped table-condensed">
+        <th>Nome</th>
+        <th>Email</th>
+        <th>Data de Nascimento</th>
+        <th>Tipo</th>
+        <th>Alterar</th>
+        <th>Remover</th>
+        <?php
+            if(!empty($colaboradores)):
+            foreach ($colaboradores as $colaborador) :
+        ?>
+            <tr>
+                <td><?=$colaborador['nome']?></td>
+                <td><?=$colaborador['email']?></td>
+                <td><input type="date" value="<?=$colaborador['datanascimento']?>" readonly="true"/></td>
+                <td><?=$colaborador['tipo_nome']?></td>
+                <td>
+                    <center>
+                            <a class="btn btn-warning" href="edita-colaborador-formulario.php?id=<?=$colaborador['id']?>">
+                                <span class="glyphicon glyphicon-edit"></span>
+                            </a>
+                    </center>
+                </td>
+                <td>
+                    <form action="remove-colaborador.php" method="POST">
+                        <center>
+                            <input type="hidden" name="id" value="<?=$colaborador['id']?>">
+                            <button class="btn btn-danger">
+                                <span class="glyphicon glyphicon-remove"></span>
+                            </button>
+                        </center>
+                    </form>
+                </td>
+            </tr>
+        <?php
+            endforeach;
+            endif;
+        ?>
+    </table>
+</div>
 <?php include "footer.php" ?>
