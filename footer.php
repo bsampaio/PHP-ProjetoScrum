8fed0a2cc88b9bbed7995450c1c134be32baff34
Breno Grillo <bsampaio@outlook.com>
2014-11-20 23:49:53 -0200

    Incluindo a possibilidade de utilizar um date time picker com o Mozilla ou IE

--- footer.php
+++ footer.php
@@ -13,3 +13,9 @@
 </body>
 
 </html>
+<!--Scripts JS-->
+<script src="//cdn.jsdelivr.net/webshim/1.14.5/polyfiller.js"></script>
+<script>
+    webshims.setOptions('forms-ext', {types: 'date'});
+webshims.polyfill('forms forms-ext');
+</script>
